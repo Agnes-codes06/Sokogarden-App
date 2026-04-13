786150c232e4f6b4b861fe69ccc2e47c632fa6b2
Agnes-codes06 <wagnes211@gmail.com>
2026-04-13 16:31:26 +0300

    Added the sign in with api intergrated

--- app/src/main/java/com/agnes/simbasokogarden/SignIn.kt
+++ app/src/main/java/com/agnes/simbasokogarden/SignIn.kt
@@ -1,12 +1,24 @@
 package com.agnes.simbasokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class SignIn : AppCompatActivity() {
+//    signin:https://agnes.alwaysdata.net/api/signin
+//        signup:https://agnes.alwaysdata.net/api/signup
+//        mpesa:https://agnes.alwaysdata.net/api/mpesa_payment
+//        products:https://agnes.alwaysdata.net/api/getproductdetails
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,46 @@ class SignIn : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val signin=findViewById<TextView>(R.id.signin_link)
+
+        signin.setOnClickListener {
+
+            val signInlink= Intent(applicationContext, SignUp ::class.java)
+            startActivity(signInlink)
+
+
+
+        }
+
+        val email=findViewById<EditText>(R.id.email)
+        val password=findViewById<EditText>(R.id.password)
+        val signinbutton=findViewById<Button>(R.id.sign)
+
+        signinbutton.setOnClickListener {
+
+            val api="https://agnes.alwaysdata.net/api/signin"
+
+//            request param is a container used to collect user details its like form datea in js
+
+            val data = RequestParams()
+
+            data.put("email",email.text.toString())
+            data.put("password",password.text.toString().trim())
+
+
+//            Api helper it delivers our data to the api
+            val helper= ApiHelper(applicationContext)
+            helper.post_login(api,data)
+
+
+
+
+
+
+        }
+
+
+
     }
 }
